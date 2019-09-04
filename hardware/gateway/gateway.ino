b991f1c02f0b649ab3c814aad46151bbc4796f3a
Henrique Martins <enrque.ns@gmail.com>
2019-09-04 17:17:35 -0300

    gateway/feature: init access point

---
+++ hardware/gateway/gateway.ino
@@ -0,0 +1,111 @@
+#include "ESP8266HTTPUpdateServer.h"
+#include "ESP8266WebServer.h"
+#include "ESP8266WiFi.h"
+#include "WiFiClient.h"
+
+#include "pages.h"
+
+/* 
+ * Equivalencia das saidas Digitais entre NodeMCU e ESP8266
+ * D0 = 16
+ * D1 = 5
+ * D2 = 4
+ * D3 = 0
+ * D4 = 2
+ * D5 = 14
+ * D6 = 12
+ * D7 = 13
+ * D8 = 15
+ * D9 = 3
+ * D10 = 1
+ */
+ 
+typedef struct {
+  float temperature;
+  float humidity;
+} Data;
+
+
+/*
+ * Access point address
+ */
+IPAddress local_IP(192,169,4,22);
+IPAddress local_gateway(192,169,4,1);
+IPAddress local_subnet(255,255,255,0);
+
+const char* local_ssid = "fibosensor";
+const char* local_password = "12345678";
+
+ESP8266WebServer server (80);
+ESP8266HTTPUpdateServer upServer;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+
+  initAP();
+  
+  connectWiFi();
+}
+
+void loop () {
+  // server.handleClient();
+}
+
+/*
+ * Starting access point network
+ */
+void initAP() {
+  Serial.print("Configuring network... ");
+  Serial.println(WiFi.softAPConfig(local_IP, local_gateway, local_subnet) ? "Ready" : "Failed!");
+
+  Serial.print("Starting WiFi network... ");
+  Serial.println(WiFi.softAP(local_ssid) ? "Ready" : "Failed!");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+  
+  initServices();
+}
+
+/*
+ * Starting html pages
+ */
+void initServices() {
+  upServer.setup(&server);
+  // server.on("/", HTTP_GET, handleRootGet);
+  // server.on("/", HTTP_POST, handleRootPost);
+  // server.on("/form", handleForm);
+  // server.on("/status", handleStatus);
+}
+
+/*
+ * Conecting to wifi network
+ */
+void connectWiFi(){
+  Serial.print("Conecting to WiFi...");
+  
+  const char *ssid = "Assert-Guest";
+  const char *password = "pass:guest!";
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.print("Connected, IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+/*
+ * get data from LoRa
+ */
+Data getData() {
+  Data data;
+  data.temperature = random(-10, 40);
+  data.humidity = random(0, 100);
+
+  return data;
+}
--- hardware/gateway/pages.h
+++ hardware/gateway/pages.h
@@ -1,3 +1,6 @@
+/*
+ * if refresh is true, refresh the page every 5 seconds
+ */
 String initPage(bool refresh) {
   String page = "<!DOCTYPE html> <html> <head>";
   page += "<meta charset=\"utf-8\" />";
@@ -23,11 +26,11 @@ String rootPage(long id, String networkWifi) {
   page += "<td>" + String(id) + "</td>";
   page += "</tr> <tr>";
 
-  page += "<td>Rede WiFi:</td>";
+  page += "<td>WiFi network:</td>";
   page += "<td>" + String(networkWifi) + "</td>";
   page += "</tr> </table>";
 
-  page += "<button>Atualizar Dados</button>";
+  page += "<button>Update data</button>";
   
   page += "</body> </html>";
 
@@ -38,11 +41,14 @@ String statusPage(float temperature, float humidity) {
   String page = initPage(true);
   
   page += "<table border=\"1px\">";
-  page += "<tr> <td> Umidade </td>";
-  page += "<td> " + String(humidity) + " </td>";
-  page += "</tr>";
-  page += "<tr> <td> Temperatura </td>";
+  
+  page += "<tr> <td> temperature </td>";
   page += "<td> " + String(temperature) + " ºC </td>";
+  
+  page += "</tr>";
+  
+  page += "<tr> <td> humidity </td>";
+  page += "<td> " + String(humidity) + " </td>";
 
   page += "</tr> </table> </body> </html>";
 
