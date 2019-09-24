b3bcb85aa97a726afc106aa5f683e3693ca3a639
Henrique Martins <enrque.ns@gmail.com>
2019-09-24 07:31:07 -0300

    endNode/chore: heltec display

--- hardware/gatewayLora/gatewayLora.ino
+++ hardware/gatewayLora/gatewayLora.ino
@@ -1,9 +1,20 @@
 #include <LoRa.h>
 #include <SPI.h>
+#include "heltec.h"
 
-#define ss 18
-#define rst 14
-#define dio0 26
+/**
+ * LoRa pinouts
+ */
+#define SS        18
+#define LORA_RST  14
+#define DIO0      26
+
+/**
+ * LCD pinouts
+ */
+#define SDA       4
+#define SCL       15
+#define LCD_RST   16
 
 String RxString;
 String RxRSSI;
@@ -14,12 +25,18 @@ void setup() {
   Serial.println("LoRa Receiver");
 
   SPI.begin(5, 19, 27, 18);
-  LoRa.setPins(ss, rst, dio0);
+  LoRa.setPins(SS, LORA_RST, DIO0);
 
+  Heltec.display->init();
+  Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_10);
+  
   if (!LoRa.begin(915E6)) {
     Serial.println("Error");
     while (1);
   }
+ 
+ 
 
   Serial.println("\nConnected");
   LoRa.receive();
@@ -27,16 +44,26 @@ void setup() {
 
 void loop() {
   int packetSize = LoRa.parsePacket();
+  String data = "";
 
   if (packetSize) {
     Serial.print("Received packet '");
 
     while (LoRa.available())
-      Serial.print((char)LoRa.read());
+      data += (char)LoRa.read();
 
+    Serial.print(data);
     Serial.print("' with RSSI ");
     RxRSSI = LoRa.packetRssi();
     Serial.println(RxRSSI);
+
+    Heltec.display->clear();
+
+    Heltec.display->drawString(0, 0, "Received " + String(packetSize) + " Bytes");
+    Heltec.display->drawString(0, 15, data);
+    Heltec.display->drawString(0, 26, "RSSi " + RxRSSI);
+
+    Heltec.display->display();
   }
 
   delay(500);
