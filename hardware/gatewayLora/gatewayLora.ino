0d0b602890feb8669faddc793918ef1f0edd5e4e
Henrique Martins <enrque.ns@gmail.com>
2019-09-27 11:45:52 -0300

    gateway/chore: LoRaReceiver code added in gatewayLora

--- hardware/gatewayLora/gatewayLora.ino
+++ hardware/gatewayLora/gatewayLora.ino
@@ -1,4 +1,3 @@
-#include <LoRa.h>
 #include <SPI.h>
 #include "heltec.h"
 
@@ -9,18 +8,18 @@
 #define LORA_RST  14
 #define DIO0      26
 
-/**
- * LCD pinouts
- */
-#define SDA       4
-#define SCL       15
-#define LCD_RST   16
+#define BAND      915E6
 
 String RxString;
 String RxRSSI;
 
+float packets_receive = 0.0;
+float packets_error = 0.0;
+
 void setup() {
   Serial.begin(9600);
+  Heltec.begin(true /*DisplayEnable*/, true /*Heltec.LoRa*/, true /*Serial*/, true /*PABOOST*/, BAND);
+
   while (!Serial);
   Serial.println("LoRa Receiver");
 
@@ -28,18 +27,70 @@ void setup() {
   LoRa.setPins(SS, LORA_RST, DIO0);
 
   Heltec.display->init();
+
   Heltec.display->flipScreenVertically();
-  Heltec.display->setFont(ArialMT_Plain_10);
-  
-  if (!LoRa.begin(915E6)) {
-    Serial.println("Error");
-    while (1);
-  }
- 
- 
+  Heltec.display->setFont(ArialMT_Plain_16);
+  Heltec.display->drawString(0, 0, "Gateway");
+
+  Heltec.display->display();
 
   Serial.println("\nConnected");
   LoRa.receive();
+}
+
+String getResponse() {
+  String response = "";
+ 
+  while(LoRa.available())
+    response += (char) LoRa.read();
+
+  return response;
+}
+
+double getTemp(String str) {
+  String temperature = "";
+
+  for(int i = 16; i < 21; i++)
+    temperature += str[i];
+
+  return temperature.toDouble();
+}
+
+double getHumi(String str) {
+  String humidity = "";
+
+  for(int i = 35; i < 40; i++)
+    humidity += str[i]; 
+
+  return humidity.toDouble();
+}
+
+bool isNum(String data) {
+  String temp(getTemp(data));
+  String humi(getHumi(data));
+
+  String strTemp = "";
+  for (int i = 16; i < 21; i++)
+    strTemp += data[i];
+
+  String strHumi = "";
+  for (int i = 35; i < 40; i++)
+    strHumi += data[i];
+
+  if(!strTemp.compareTo(temp) && !strHumi.compareTo(humi))
+    return true;
+
+  return false;
+}
+
+bool isPacketCorrect(String data, double temperature, double humidity) {
+  String packet = "{\"Temperature\": ";
+  packet += String(temperature);
+  packet += ", \"Humidity\": ";
+  packet += String(humidity);
+  packet += "}";
+
+  return packet.compareTo(data) ? false : true;
 }
 
 void loop() {
@@ -47,13 +98,21 @@ void loop() {
   String data = "";
 
   if (packetSize) {
+    packets_receive++;
     Serial.print("Received packet '");
 
-    while (LoRa.available())
-      data += (char)LoRa.read();
+    String data = getResponse();
 
-    Serial.print(data);
-    Serial.print("' with RSSI ");
+    if (!isPacketCorrect(data, getTemp(data), getHumi(data)))
+      packets_error++;
+
+    Serial.println(data);
+    Serial.print("Packets receive: ");
+    Serial.print(packets_receive);
+    Serial.print(", Packets error: ");
+    Serial.print((packets_error/packets_receive)*100);
+    Serial.print("%");
+    Serial.print(", RSSI: ");
     RxRSSI = LoRa.packetRssi();
     Serial.println(RxRSSI);
 
