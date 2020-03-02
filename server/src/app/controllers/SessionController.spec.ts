3631cefcca901635801ec390a0e80efe16c41fb0
Henrique Miranda <enrque.ns@gmail.com>
2020-03-02 10:54:25 -0300

    :sparkles: feat(server): authetication route

---
+++ server/src/app/controllers/SessionController.spec.ts
@@ -0,0 +1,76 @@
+import request from 'supertest';
+import app from '../../app';
+
+import factory from '../../util/tests/factories';
+import User from '../models/User';
+
+import { cleanPostgres } from '../../util/tests/cleanDB';
+
+describe('Session', () => {
+  const path = '/sessions';
+
+  beforeEach(async () => {
+    await cleanPostgres();
+  });
+
+  it('should log in with correct data', async () => {
+    const password = '123';
+
+    const { id, email, name } = await factory.create<User>('User', {
+      password,
+    });
+
+    const response = await request(app)
+      .post(path)
+      .send({
+        email,
+        password,
+      });
+
+    expect(response.status).toBe(200);
+
+    expect(response.body).toHaveProperty('token');
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        user: {
+          id,
+          name,
+          email,
+        },
+      }),
+    );
+  });
+
+  it("should return an error if provide a user doesn't exist", async () => {
+    const { email, password } = await factory.attrs<User>('User');
+
+    const response = await request(app)
+      .post(path)
+      .send({
+        email,
+        password,
+      });
+
+    expect(response.status).toBe(401);
+
+    expect(response.body).toHaveProperty('error');
+  });
+
+  it('should return an error if provided an invalid password', async () => {
+    const password = '123';
+
+    const { email } = await factory.create<User>('User', { password });
+
+    const response = await request(app)
+      .post(path)
+      .send({
+        email,
+        password: '1',
+      });
+
+    expect(response.status).toBe(401);
+
+    expect(response.body).toHaveProperty('error');
+  });
+});
---
+++ server/src/app/controllers/SessionController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req: Request, res: Response) {
+    const { email, password } = req.body as User;
+
+    /**
+     * Check if user exists
+     */
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+    const { secret, expiresIn } = authConfig;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, secret, {
+        expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
