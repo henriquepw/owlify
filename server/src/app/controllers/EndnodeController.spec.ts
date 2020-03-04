44e78909d3213b1c9ecef4d347b5e126d435aa6a
Henrique Miranda <enrque.ns@gmail.com>
2020-03-04 17:35:59 -0300

    :white_check_mark: test(server): create describes for endnode tests

---
+++ server/src/app/controllers/EndnodeController.spec.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+
+describe('Endnode', () => {
+  describe('GET /endnodes', () => {});
+
+  describe('GET /endnodes/:gatewayId', () => {});
+
+  describe('POST /endnodes', () => {});
+
+  describe('PUT /endnodes/:id', () => {});
+
+  describe('DELETE /endnodes/:id', () => {});
+});
