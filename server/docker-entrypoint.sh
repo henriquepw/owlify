29230f555964c30662db9128f955940a55587799
Henrique Miranda <enrque.ns@gmail.com>
2020-03-10 22:29:14 -0300

    :bulb: chore(server): create docker-compose.prod

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -2,4 +2,13 @@
 
 dockerize -wait tcp://owlify_pg:5432 -wait tcp://owlify_influx:8086 -timeout 300s
 
-yarn dev
+yarn migrate
+
+if [ "$1" == "production" ];
+then
+  yarn build
+  yarn start
+  yarn pm2 monit
+else
+  yarn dev
+fi
