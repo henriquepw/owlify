ac56285c6f3ac2d63b2721477f627662a2da5063
Henrique Miranda <enrque.ns@gmail.com>
2020-05-18 12:33:54 -0300

    :bulb: chore(server): creating service for authenticate user

--- server/src/modules/users/infra/http/controllers/SessionsController.ts
+++ server/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,50 +1,20 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { sign } from 'jsonwebtoken';
 
-import authConfig from '@config/auth';
-
-import User from '@modules/users/infra/typeorm/entities/User';
+import { container } from 'tsyringe';
+import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 class SessionsController {
   public async store(req: Request, res: Response): Promise<Response> {
-    const usersRepository = getRepository(User);
+    const { email, password } = req.body;
 
-    const {
+    const authenticateUser = container.resolve(AuthenticateUserService);
+
+    const { user, token } = await authenticateUser.execute({
       email,
-      /* password */
-    } = req.body as User;
-
-    /**
-     * Check if user exists
-     */
-    const user = await usersRepository.findOne({ where: { email } });
-
-    if (!user) {
-      return res.status(401).json({ error: 'User not found' });
-    }
-
-    // if (!(await user.checkPassword(password))) {
-    //   return res.status(401).json({ error: 'Password does not match' });
-    // }
-
-    const { id, name } = user;
-
-    const { secret, expiresIn } = authConfig.jwt;
-
-    const token = sign({}, secret, {
-      subject: user.id,
-      expiresIn,
+      password,
     });
 
-    return res.json({
-      token,
-      user: {
-        id,
-        name,
-        email,
-      },
-    });
+    return res.json({ user, token });
   }
 }
 
---
+++ server/src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,74 @@
+import { injectable, inject } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
+
+import authConfig from '@config/auth';
+
+import AppError from '@shared/Errors/AppError';
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+export interface IRequest {
+  email: string;
+  password: string;
+}
+
+export interface IResponse {
+  token: string;
+  user: {
+    id: string;
+    name: string;
+    email: string;
+  };
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    /**
+     * Check if user exists
+     */
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password.', 401);
+    }
+
+    /**
+     * Check if password matched
+     */
+    const isPasswordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!isPasswordMatched) {
+      throw new AppError('Incorrect email/password.', 401);
+    }
+
+    /**
+     * Create Token
+     */
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      token,
+      user,
+    };
+  }
+}
+
+export default AuthenticateUserService;
