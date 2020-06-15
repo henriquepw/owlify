55b7fdae8dd4c5b28b593e43a997e7cb29777897
GLEDSONS831 <glegogle84@gmail.com>
2020-06-15 19:55:09 -0300

    :bulb: chore(mobile): add new emojis commit

--- mobile/.cz-config.js
+++ mobile/.cz-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   types: [
-    { value: ':pencil', name: ':pencil:    A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: ':sparkles', name: 'feat:    A new feature' },
+    { value: ':beetle', name: 'fix:      A bug fix' },
     { value: 'docs', name: 'docs:     Documentation only changes' },
     {
       value: 'style',
@@ -9,7 +9,7 @@ module.exports = {
         'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
     },
     {
-      value: 'refactor',
+      value: ':hammer',
       name:
         'refactor: A code change that neither fixes a bug nor adds a feature',
     },
@@ -19,7 +19,7 @@ module.exports = {
     },
     { value: 'test', name: 'test:     Adding missing tests' },
     {
-      value: 'chore',
+      value: ':bulb',
       name:
         'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
     },
