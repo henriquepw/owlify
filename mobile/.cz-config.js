d678dd0b7e0f8cf189808ebda620c11d60a16436
Henrique Miranda <enrque.ns@gmail.com>
2020-06-16 22:15:27 -0300

    :bulb: chore(mobile): config cz scopes

--- mobile/.cz-config.js
+++ mobile/.cz-config.js
@@ -1,38 +1,45 @@
 module.exports = {
   types: [
-    { value: ':sparkles: feat', name: 'feat:    A new feature' },
-    { value: ':beetle: fix', name: 'fix:      A bug fix' },
-    { value: ':page_facing_up: docs', name: 'docs:     Documentation only changes' },
-    {
-      value: ':art: style',
-      name:
-        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
-    },
-    {
-      value: ':hammer: refactor',
-      name:
-        'refactor: A code change that neither fixes a bug nor adds a feature',
-    },
-    {
-      value: ':checkered_flag: perf',
-      name: 'perf:     A code change that improves performance',
-    },
-    { value: ':white_check_mark: test', name: 'test:     Adding missing tests' },
     {
       value: ':bulb: chore',
       name:
         'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
     },
+    { value: ':sparkles: feat', name: 'feat:     A new feature' },
+    { value: ':beetle: fix', name: 'fix:      A bug fix' },
+    { value: ':pencil: docs', name: 'docs:     Documentation only changes' },
+    {
+      value: ':lipstick: style',
+      name:
+        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: ':checkered_flag: perf',
+      name: 'perf:     A code change that improves performance',
+    },
+    {
+      value: ':white_check_mark: test',
+      name: 'test:     Adding missing tests',
+    },
     { value: ':back: revert', name: 'revert:   Revert to a commit' },
-    { value: ':building_construction: build', name: 'build:      Work in progress' },
-    {value: ':heavy_plus_sign: ci', name: 'ci: ci related changes'}
+    {
+      value: ':building_construction: build',
+      name: 'build:    Work in progress',
+    },
+    { value: ':heavy_plus_sign: ci', name: 'ci:       ci related changes' },
+    {
+      value: ':hammer: refactor',
+      name:
+        'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
   ],
 
   scopes: [
-    { name: 'accounts' },
-    { name: 'admin' },
-    { name: 'exampleScope' },
-    { name: 'changeMe' },
+    { name: 'mobile' },
+    { name: 'server' },
+    { name: 'endnode' },
+    { name: 'gateway' },
+    { name: 'analytics' },
   ],
 
   allowTicketNumber: false,
@@ -40,18 +47,6 @@ module.exports = {
   ticketNumberPrefix: 'TICKET-',
   ticketNumberRegExp: '\\d{1,5}',
 
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-    scopeOverrides: {
-      fix: [
-        {name: 'merge'},
-        {name: 'style'},
-        {name: 'e2eTest'},
-        {name: 'unitTest'}
-      ]
-    },
-    */
-  // override the messages, defaults are as follows
   messages: {
     type: "Select the type of change that you're committing:",
     scope: '\nDenote the SCOPE of this change (optional):',
@@ -73,7 +68,4 @@ module.exports = {
 
   // limit subject length
   subjectLimit: 100,
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true, // default is false
 };
