9e053ed3ffebb0d30a41ed45f34d8fe227eff8ae
Henrique Miranda <enrque.ns@gmail.com>
2020-06-28 15:12:48 -0300

    :lipstick: style(mobile): add import alias

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@hooks": "./src/hooks",
+          "@pages": "./src/pages",
+          "@routes": "./src/routes",
+          "@services": "./src/services",
+          "@styles": "./src/styles",
+          "@utils": "./src/utils",
+        },
+      },
+    ],
+  ],
 };
