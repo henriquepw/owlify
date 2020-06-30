7dfb5d9f6c3d1dfec128609c850efe5c87d75243
GledsonS831 <glegogle84@gmail.com>
2020-06-30 15:42:53 -0300

    :bulb: chore(mobile): add icon in button component

--- mobile/src/components/atoms/Button/index.tsx
+++ mobile/src/components/atoms/Button/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+/* import { Text } from 'react-native'; */
 
 import * as S from './styles';
 
@@ -9,12 +10,12 @@ interface propsButton {
   hasIcon?: boolean;
 }
 
-const Button: React.FC<propsButton> = ({ type, text, hasIcon }) => {
+const Button: React.FC<propsButton> = ({ type, text, hasIcon = false }) => {
   return (
     <S.Container optionsButtom={type} hasIcon={hasIcon}>
       {hasIcon ? (
         <S.Icon>
-          <Text>Icone aqui</Text>
+          <Icon name="shake" />
         </S.Icon>
       ) : null}
       <S.Content>{text}</S.Content>
--- mobile/storybook/stories/atoms/Button.stories.tsx
+++ mobile/storybook/stories/atoms/Button.stories.tsx
@@ -5,13 +5,7 @@ import { storiesOf } from '@storybook/react-native';
 import Button from '../../../src/components/atoms/Button';
 
 storiesOf('Button', module)
-  .add('DEFAULT', () => (
-    <Button type="isFilled" text="DEFAULT" hasIcon={false} />
-  ))
-  .add('DISABLED', () => (
-    <Button type="isFocused" text="DISABLED" hasIcon={false} />
-  ))
-  .add('ATTENTION', () => (
-    <Button type="isErrored" text="ATTENTION" hasIcon={false} />
-  ))
+  .add('DEFAULT', () => <Button type="isFilled" text="DEFAULT" />)
+  .add('DISABLED', () => <Button type="isFocused" text="DISABLED" />)
+  .add('ATTENTION', () => <Button type="isErrored" text="ATTENTION" />)
   .add('WITH ICON', () => <Button type="isFilled" text="WITH ICON" hasIcon />);
