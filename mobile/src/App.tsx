b9ed5e0d7703048a107923a31c080f489880647a
Henrique Miranda <enrque.ns@gmail.com>
2020-07-10 17:57:02 -0300

    :checkered_flag: perf(mobile): use react-native-screens to optimize memory usage and performance

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -2,11 +2,15 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { enableScreens } from 'react-native-screens';
 
 import AppProvider from '@templates/AppProvider';
 
 import Storebook from '../storybook';
 import Routes from './routes';
+
+// Bringing the native navigation component
+enableScreens();
 
 const App: React.FC = () => {
   return (
