25a22b003766f26a2fdf32e2acde93a2b112ee3f
Henrique Miranda <enrque.ns@gmail.com>
2020-07-10 11:54:48 -0300

    :beetle: fix(mobile): add missing module parameter in stories

--- mobile/storybook/stories/atoms/Icon.stories.tsx
+++ mobile/storybook/stories/atoms/Icon.stories.tsx
@@ -8,13 +8,13 @@ import { backgroundDark } from '../../decorators';
 
 const size = 180;
 
-storiesOf('Icon')
+storiesOf('Icon', module)
   .add('Default', () => <Icon name="gateway" size={size} />)
   .add('Wihout Background', () => (
     <Icon name="gateway" background={false} size={size} />
   ));
 
-storiesOf('Icon light color')
+storiesOf('Icon light color', module)
   .addDecorator(backgroundDark)
   .add('Light color', () => <Icon name="gateway" color="light" size={size} />)
   .add('Light color Wihout Background', () => (
