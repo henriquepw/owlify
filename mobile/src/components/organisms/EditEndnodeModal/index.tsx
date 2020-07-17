4737d80ede224f4d20bc0fb8ecb0ec670bc06da1
Henrique Miranda <enrque.ns@gmail.com>
2020-07-17 17:33:40 -0300

    :bulb: chore(mobile): init EnditEndnodeModal component

---
+++ mobile/src/components/organisms/EditEndnodeModal/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+
+import api from '@services/api';
+import { trigger } from 'swr';
+import * as Yup from 'yup';
+
+import Input from '@atoms/Input';
+
+import { useForm } from '@hooks';
+
+import { Endnode } from '@utils/interfaces';
+
+import * as S from './styles';
+
+interface EditGatewayModalProps {
+  isVisible: boolean;
+  endnode: Endnode;
+  onCancel: () => void;
+}
+
+interface FormData {
+  name: string;
+  room: string;
+}
+
+const validationSchema = {
+  name: Yup.string().required('Name is required'),
+  room: Yup.string().required('Room is required'),
+};
+
+const EditGatewayModal: React.FC<EditGatewayModalProps> = ({
+  isVisible,
+  endnode,
+  onCancel,
+}) => {
+  const { formRef, validateForm, submitForm } = useForm(validationSchema);
+
+  async function handleSubmit({ name, room }: FormData): Promise<void> {
+    const isValid = validateForm({ name, room });
+
+    if (!isValid) return;
+
+    try {
+      if (endnode.name !== name && endnode.room !== room) {
+        // TODO: mutate the endnode.
+
+        await api.put(`/endnodes/${endnode.id}`, { name, room });
+
+        trigger('/endnodes');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    onCancel();
+  }
+
+  return (
+    <Modal isVisible={isVisible} coverScreen={false}>
+      <S.UpdateForm ref={formRef} onSubmit={handleSubmit} initialData={endnode}>
+        <S.Title>New location</S.Title>
+
+        <Input
+          name="name"
+          icon="user"
+          placeholder="Name"
+          returnKeyType="send"
+          onSubmitEditing={submitForm}
+        />
+
+        <Input
+          name="room"
+          icon="map-pin"
+          placeholder="Room"
+          returnKeyType="send"
+          onSubmitEditing={submitForm}
+        />
+
+        <S.ButtonContainer>
+          <S.Button text="Cancel" onPress={onCancel} />
+          <S.Button text="Update" onPress={submitForm} />
+        </S.ButtonContainer>
+      </S.UpdateForm>
+    </Modal>
+  );
+};
+
+export default EditGatewayModal;
---
+++ mobile/src/components/organisms/EditEndnodeModal/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+import { Form } from '@unform/mobile';
+
+import AtomButton, { ButtonProps } from '@atoms/Button';
+
+export const UpdateForm = styled(Form)`
+  background: ${({ theme }) => theme.colours.card};
+
+  border-radius: 10px;
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colours.active};
+
+  margin-bottom: 16px;
+`;
+
+export const ButtonContainer = styled.View`
+  margin-top: 24px;
+  flex-direction: row;
+`;
+
+export const Button = styled(AtomButton)<ButtonProps>`
+  flex: 1;
+
+  margin-left: ${({ text }) => (text === 'Cancel' ? 24 : 0)}px;
+  background: ${({ theme, text }) =>
+    text === 'Cancel' ? theme.colours.withoutFocus : theme.colours.active};
+`;
