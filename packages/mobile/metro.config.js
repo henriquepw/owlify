cfd395882d0294bcb962bceac159bdee01a55c68
Henrique Miranda <enrque.ns@gmail.com>
2020-07-23 12:21:07 -0300

    :bulb: chore: config global eslint

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 module.exports = {
   projectRoot: path.resolve(__dirname, '.'),
-  watchFolders: [path.resolve(__dirname, '../node_modules')],
+  watchFolders: [path.resolve(__dirname, '../../node_modules')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
