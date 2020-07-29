09406910b1fe51805c2e7d55b80033cc557294ee
Henrique Miranda <enrque.ns@gmail.com>
2020-07-29 19:59:38 -0300

    :bulb: chore(mobile): create a env file

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+import { API_URL } from '../../env.json';
+
 const api = axios.create({
-  baseURL: 'http://192.168.0.106:3333',
+  baseURL: API_URL,
 });
 
 export default api;
